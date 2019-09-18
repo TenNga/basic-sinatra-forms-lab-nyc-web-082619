9293e33fbf513bdecc5e4609cf94f34b576ca561
Kevin Karma <kevinkarma55@gmail.com>
2019-09-18 16:46:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,6 +11,13 @@ class App < Sinatra::Base
     end
 
     post "/team" do
+      <%team_name = @team_detials[:name]%>
+    <%coach = @team_detials[:coach]%>
+    <%point_guard = @team_detials[:pg]%>
+    <%shooting_guard = @team_detials[:sg]%>
+    <%power_forward = @team_detials[:pf]%>
+    <%small_forward = @team_detials[:sf]%>
+    <%center = @team_detials[:c]%>
         @team_details = params
         erb :team
     end
